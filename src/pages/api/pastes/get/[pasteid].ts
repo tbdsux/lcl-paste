44efd965af4675beb5cc465fe650be1dca399438
TheBoringDude <iamcoderx@gmail.com>
2021-04-06 20:50:22 +0800

    fix: do not require authentication

--- src/pages/api/pastes/get/[pasteid].ts
+++ src/pages/api/pastes/get/[pasteid].ts
@@ -1,15 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { withApiAuthRequired } from '@auth0/nextjs-auth0';
 
 import { PasteModel } from '@lib/models/paste';
 import methodHandler from '@lib/middleware/methods';
+
+import { autoString } from '@utils/funcs';
 
 const getPaste = async (req: NextApiRequest, res: NextApiResponse) => {
   const { pasteid } = req.query;
 
   const p = new PasteModel();
   // automatically join all strings if array
-  const q = await p.getPaste(Array.isArray(pasteid) ? pasteid.join() : pasteid);
+  const q = await p.getPaste(autoString(pasteid));
 
   if (q) {
     return res.status(200).json(q);
@@ -17,4 +18,4 @@ const getPaste = async (req: NextApiRequest, res: NextApiResponse) => {
   return res.status(500).json({ error: 'Internal Server Error' });
 };
 
-export default methodHandler(withApiAuthRequired(getPaste), ['GET']);
+export default methodHandler(getPaste, ['GET']);
