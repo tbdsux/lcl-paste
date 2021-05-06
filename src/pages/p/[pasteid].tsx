08a229e72b8173f2f9f3a50cad15f6a74b668039
TheBoringDude <iamcoderx@gmail.com>
2021-05-06 20:13:47 +0800

    change pasteId page (/p/{pasteid}) - ui design change - add user author and publicity - buttons: copy & redirect to carbon.now.sh

--- src/pages/p/[pasteid].tsx
+++ src/pages/p/[pasteid].tsx
@@ -29,6 +29,9 @@ export default function ViewPaste() {
     return <Error statusCode={paste.code} />;
   }
 
+  const getPrivacy = paste.data.paste.isPrivate ? 'private' : 'public';
+  const getUser = paste.data.paste.ownedByUsername ? paste.data.paste.ownedByUsername : 'anonymous';
+
   return (
     <Layout title={paste.data.paste.filename}>
       <Head>
@@ -36,33 +39,76 @@ export default function ViewPaste() {
       </Head>
 
       <div className="w-5/6 mx-auto my-8">
-        <div className="py-2 flex items-center justify-between">
-          <div>
-            <h4 id="paste-filename" className="text-primary-500 text-lg font-bold tracking-wide">
-              {paste.data.paste.filename}
-            </h4>
-            <p id="paste-description" className="ml-2 text-secondary-500 mt-1">
-              {paste.data.paste.description}
-            </p>
-          </div>
-          <div className="inline-flex text-sm">
-            {user && user?.sub == paste.data.user?.user && (
-              <Link href={`/user/pastes/update/${paste.data.id}/${paste.data.paste.pasteId}`}>
-                <a className="mr-2 bg-primary-400 hover:bg-primary-500 p-1 text-white rounded-md">update</a>
+        <div className="p-6 border border-primary-300 rounded-lg">
+          <div className="pb-2 flex flex-col sm:flex-row items-start sm:items-center justify-between">
+            <div>
+              <h4 id="paste-filename" className="text-primary-500 text-lg font-bold tracking-wider">
+                {paste.data.paste.filename}
+              </h4>
+              <p id="paste-description" className="ml-2 text-secondary-500 mt-1 tracking-wide">
+                {paste.data.paste.description}
+              </p>
+            </div>
+            <div className="inline-flex text-sm mt-3 sm:mt-0">
+              {user && user?.sub == paste.data.user?.user && (
+                <Link href={`/user/pastes/update/${paste.data.id}/${paste.data.paste.pasteId}`}>
+                  <a className="mr-2 bg-primary-400 hover:bg-primary-500 p-1 text-white rounded-md">update</a>
+                </Link>
+              )}
+              <Link href={`/api/raw/${paste.data.paste.pasteId}/${paste.data.paste.filename}`}>
+                <a
+                  className="bg-secondary-400 hover:bg-secondary-500 text-white py-1 px-3 rounded-md tracking-wide"
+                  target="_blank"
+                >
+                  raw
+                </a>
               </Link>
-            )}
-            <Link href={`/api/raw/${paste.data.paste.pasteId}/${paste.data.paste.filename}`}>
-              <a
-                className="bg-secondary-400 hover:bg-secondary-500 text-white py-1 px-3 rounded-md tracking-wide"
-                target="_blank"
-              >
-                raw
-              </a>
-            </Link>
+            </div>
           </div>
-        </div>
-        <div className="border border-secondary-200 p-4 rounded-md">
-          <Highlight className={`text-sm ${paste.data.paste.codeLanguage}`}>{paste.data.paste.content}</Highlight>
+          <hr className="my-4" />
+          <div className="p-4 rounded-md shadow-lg border border-secondary-200 relative">
+            <div className="absolute top-1 right-1 inline-flex items-center">
+              {/* redirect to carbon */}
+              <a
+                title="Create Carbon Image"
+                className="text-primary-400 hover:text-primary-500"
+                target="_blank"
+                href={`https://carbon.now.sh/?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=javascript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=${encodeURI(
+                  paste.data.paste.content
+                )}`}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+              {/* end redirect to carbon */}
+              {/* copy button */}
+              <button
+                onClick={() => {
+                  navigator.clipboard.writeText(paste.data.paste.content);
+                }}
+                className="text-secondary-400 hover:text-secondary-500"
+                title="Copy Code"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z" />
+                  <path d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z" />
+                </svg>
+              </button>
+              {/* end copy button */}
+            </div>
+            <Highlight className={`text-sm ${paste.data.paste.codeLanguage}`}>{paste.data.paste.content}</Highlight>
+          </div>
+
+          <div className="flex items-center justify-between text-sm mt-6">
+            <p className="bg-secondary-400 text-secondary-50 p-1">{getPrivacy}</p>
+            <p className="text-secondary-600 underline">@{getUser}</p>
+          </div>
         </div>
       </div>
     </Layout>
