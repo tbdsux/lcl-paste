b47a1db5da327f3738345f7499ca30edcf36d580
TheBoringDude <iamcoderx@gmail.com>
2021-05-11 18:12:18 +0800

    add nextjs-progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0';
 
 import '../styles/tailwind.css';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
+      <NextNProgress height={4} color="#f59e0b"  />
       <Component {...pageProps} />
     </UserProvider>
   );
