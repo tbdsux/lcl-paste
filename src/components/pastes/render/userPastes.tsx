ef2a0b38fca28219e834a5c86638509a8ad3859a
TheBoringDude <iamcoderx@gmail.com>
2021-05-18 15:05:20 +0800

    new look in user pastes page

--- src/components/pastes/render/userPastes.tsx
+++ src/components/pastes/render/userPastes.tsx
@@ -1,6 +1,8 @@
 import useSWR from 'swr';
+import Link from 'next/link';
 
-import { BlockPasteInfo } from '../Block';
+import { UserActions } from '@components/pastes/userActions';
+import { Details } from '@components/pastes/details';
 
 import { PasteQueryResponse } from '@utils/interfaces/query';
 import { ApiGetUserPastes } from 'pages/api/pastes/user';
@@ -17,11 +19,38 @@ export const RenderUserPastes = () => {
   }
 
   return (
-    <ul className="mt-6">
+    <ul className="mt-6 grid grid-cols-2 gap-4">
       {pastes.data?.map((paste: PasteQueryResponse, index) => (
-        <section key={index}>
-          <BlockPasteInfo paste={paste} isUserPage={true} /> <hr className="border-secondary-100 my-1" />
-        </section>
+        <li
+          className="relative border-2 border-secondary-100 py-4 px-6 rounded hover:border-primary-300 flex flex-col"
+          key={index}
+        >
+          <Link href={`/p/${paste.data.pasteId}`}>
+            <a>
+              <div className="flex flex-col">
+                <h4 className="font-bold text-primary-600 tracking-wide">{paste.data.filename} </h4>
+                <span className="text-secondary-500 text-sm font-normal mt-1 truncate">
+                  {paste.data.description ? `(${paste.data.description})` : <br />}
+                </span>
+              </div>
+            </a>
+          </Link>
+
+          <div className="text-secondary-500 absolute top-2 right-2">
+            <UserActions refid={paste.ref['@ref'].id} pasteid={paste.data.pasteId} />
+          </div>
+
+          <div className="mt-2 text-secondary-400">
+            <Details
+              username={paste.data.ownedByUsername}
+              createdDate={paste.data.createdDate}
+              updated={paste.data.updated}
+              isPrivate={paste.data.isPrivate}
+              updatedDate={paste.data.updatedDate}
+              isUserPage={true}
+            />
+          </div>
+        </li>
       ))}
     </ul>
   );
--- src/pages/latest.tsx
+++ src/pages/latest.tsx
@@ -1,22 +1,14 @@
-import useSWR from 'swr';
-
 import Layout from '@components/Layout';
-import { BlockPasteInfo } from '@components/pastes/Block';
-import { Loading } from '@components/Loading';
-
-import { PasteQueryResponse } from '@utils/interfaces/query';
-import { ApiGetLatestResponse } from './api/pastes/latest';
-import Error from 'next/error';
 import { RenderLatestPastes } from '@components/pastes/render/latestPastes';
 
 export default function Latest() {
   return (
     <Layout title="Latest Pastes">
       <div className="py-8 w-5/6 mx-auto">
-          <h3 className="font-bold tracking-wide text-xl">Latest</h3>
+        <h3 className="font-bold tracking-wide text-xl">Latest</h3>
 
-          <RenderLatestPastes />
-        </div>
+        <RenderLatestPastes />
+      </div>
     </Layout>
   );
 }
