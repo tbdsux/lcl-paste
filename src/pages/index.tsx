bc8bd4be0728ba7c04d9dcaa6b2d0adf9e35934b
TheBoringDude <iamcoderx@gmail.com>
2021-05-18 14:28:31 +0800

    page editor header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import Layout from '@components/Layout';
 export default function Home() {
   return (
     <Layout title="Welcome ">
+      <div className="w-5/6 mx-auto mt-8">
+        <p className="tracking-wide text-primary-500 text-lg">Create A New Paste</p>
+      </div>
       <MainEditor />
     </Layout>
   );
--- src/pages/user/pastes/update/[refid]/[pasteid].tsx
+++ src/pages/user/pastes/update/[refid]/[pasteid].tsx
@@ -36,6 +36,11 @@ export default withPageAuthRequired(function UserPage() {
 
   return (
     <Layout title={`${paste.data.filename} - Update`}>
+      <div className="w-5/6 mx-auto mt-8">
+        <p className="tracking-wide text-primary-500 text-lg">
+          Update <strong>{paste.data.filename}</strong>
+        </p>
+      </div>
       {paste.data && <MainEditor update={true} data={paste.data} refid={autoString(refid)} />}
     </Layout>
   );
