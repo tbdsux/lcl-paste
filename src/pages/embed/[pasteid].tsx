ba6c25fa4dece80ff14be4e8c1a53f34cf2e032d
TheBoringDude <iamcoderx@gmail.com>
2021-06-03 11:23:16 +0800

    make paste embedding work - new page `embed/` - separate paste container as a component

--- src/pages/embed/[pasteid].tsx
+++ src/pages/embed/[pasteid].tsx
@@ -7,16 +7,13 @@ import { jsonify, joinString } from '@ootiq/blank';
 import { GetPasteByIdQuery } from '@utils/interfaces/query';
 import { GetPasteHandler } from '@functions/getPaste';
 import { PasteContainer } from '@components/pastes/page';
-import Layout from '@components/Layout';
-import { CopyEmbed } from '@components/pastes/embed';
-import { useRef } from 'react';
 
-type ViewPasteProps = {
+type GetPasteProps = {
   pasteid: string;
   initialPaste: GetPasteByIdQuery;
 };
 
-export const getServerSideProps: GetServerSideProps<ViewPasteProps> = async ({ req, res, params }) => {
+export const getServerSideProps: GetServerSideProps<GetPasteProps> = async ({ req, res, params }) => {
   const { pasteid } = params;
 
   const q = await GetPasteHandler(req, res, joinString(pasteid));
@@ -29,19 +26,11 @@ export const getServerSideProps: GetServerSideProps<ViewPasteProps> = async ({ r
   };
 };
 
-export default function ViewPaste({ pasteid, initialPaste }: ViewPasteProps) {
+export default function EmbedPaste({ pasteid, initialPaste }: GetPasteProps) {
   // get response
   const { data: paste } = useSWR<GetPasteByIdQuery>(pasteid ? `/api/pastes/get/${pasteid}` : null, {
     initialData: initialPaste
   });
 
-  const pasteContainerRef = useRef<HTMLDivElement>(null);
-
-  return (
-    <Layout title={paste.error ? paste.description : paste.data.paste.filename}>
-      <PasteContainer paste={paste} pasteContainerRef={pasteContainerRef} />
-
-      <CopyEmbed pasteid={pasteid} pasteContainerRef={pasteContainerRef} />
-    </Layout>
-  );
+  return <PasteContainer paste={paste} />;
 }
