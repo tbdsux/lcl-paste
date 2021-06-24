f6e81008f2185e9aa947b8d3a501ccc4912e3bdc
TheBoringDude <iamcoderx@gmail.com>
2021-06-24 22:01:11 +0800

    show texts if no placeholder yet

--- src/components/pastes/render/latestPastes.tsx
+++ src/components/pastes/render/latestPastes.tsx
@@ -20,53 +20,59 @@ export const RenderLatestPastes = ({ initialPastes }: RenderLatestPastesProps) =
 
   return (
     <ul className="mt-6">
-      {pastes.data?.map((paste: PasteQueryResponse, index) => (
-        <li className="my-3" key={index}>
-          <Link href={`/p/${paste.data.pasteId}`}>
-            <a
-              className="relative col-span-11 border border-secondary-300 py-3 px-6 flex flex-col md:flex-row items-start md:items-center justify-between rounded hover:border-primary-500"
-              title="View paste"
-            >
-              {isNew(paste.data.createdDate) && (
-                <span
-                  title="Paste is newly created"
-                  className="absolute -top-2 -left-2 bg-primary-500 text-white rounded-md p-1 text-xs"
-                >
-                  new
-                </span>
-              )}
-
-              {/* start paste info */}
-              <div className="w-full md:w-1/2 justify-start inline-flex">
-                <h4 className="truncate">
-                  {paste.data.filename}{' '}
-                  <span className="text-secondary-500 text-sm">
-                    {paste.data.description ? `(${paste.data.description})` : null}
+      {pastes.data.length > 0 ? (
+        pastes.data.map((paste: PasteQueryResponse, index) => (
+          <li className="my-3" key={index}>
+            <Link href={`/p/${paste.data.pasteId}`}>
+              <a
+                className="relative col-span-11 border border-secondary-300 py-3 px-6 flex flex-col md:flex-row items-start md:items-center justify-between rounded hover:border-primary-500"
+                title="View paste"
+              >
+                {isNew(paste.data.createdDate) && (
+                  <span
+                    title="Paste is newly created"
+                    className="absolute -top-2 -left-2 bg-primary-500 text-white rounded-md p-1 text-xs"
+                  >
+                    new
                   </span>
-                </h4>
-              </div>
-              {/* end paste info */}
+                )}
 
-              {/* start paste other details */}
-              <div className="w-full md:w-1/2 mt-1 md:mt-0 justify-end inline-flex items-center text-secondary-500">
-                <div className="text-sm text-secondary-400 md:ml-8 inline-flex flex-col md:flex-row items-end md:items-center">
-                  <span title="author">@{paste.data.ownedByUsername ? paste.data.ownedByUsername : 'anonymous'}</span>{' '}
-                  <span className="ml-1 text-xs text-secondary-300" title="paste created date">
-                    {[
-                      `${
-                        paste.data.updated
-                          ? `${new Date(paste.data.updatedDate).toUTCString()}-updated`
-                          : new Date(paste.data.createdDate).toUTCString()
-                      }`
-                    ]}
-                  </span>
+                {/* start paste info */}
+                <div className="w-full md:w-1/2 justify-start inline-flex">
+                  <h4 className="truncate">
+                    {paste.data.filename}{' '}
+                    <span className="text-secondary-500 text-sm">
+                      {paste.data.description ? `(${paste.data.description})` : null}
+                    </span>
+                  </h4>
                 </div>
-              </div>
-              {/* end paste other details */}
-            </a>
-          </Link>
-        </li>
-      ))}
+                {/* end paste info */}
+
+                {/* start paste other details */}
+                <div className="w-full md:w-1/2 mt-1 md:mt-0 justify-end inline-flex items-center text-secondary-500">
+                  <div className="text-sm text-secondary-400 md:ml-8 inline-flex flex-col md:flex-row items-end md:items-center">
+                    <span title="author">@{paste.data.ownedByUsername ? paste.data.ownedByUsername : 'anonymous'}</span>{' '}
+                    <span className="ml-1 text-xs text-secondary-300" title="paste created date">
+                      {[
+                        `${
+                          paste.data.updated
+                            ? `${new Date(paste.data.updatedDate).toUTCString()}-updated`
+                            : new Date(paste.data.createdDate).toUTCString()
+                        }`
+                      ]}
+                    </span>
+                  </div>
+                </div>
+                {/* end paste other details */}
+              </a>
+            </Link>
+          </li>
+        ))
+      ) : (
+        <p className="text-secondary-600 tracking-wide text-sm">
+          No latest pastes yet! Try to create one and check it here!
+        </p>
+      )}
     </ul>
   );
 };
--- src/components/pastes/render/userPastes.tsx
+++ src/components/pastes/render/userPastes.tsx
@@ -16,44 +16,50 @@ export const RenderUserPastes = () => {
 
   return (
     <ul className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-2 lg:gap-4">
-      {pastes.data?.map((paste: PasteQueryResponse, index) => (
-        <li
-          className="relative border-2 border-secondary-200 py-4 px-6 rounded hover:border-primary-300 flex flex-col"
-          key={index}
-        >
-          <Link href={`/p/${paste.data.pasteId}`}>
-            <a>
-              <div className="flex flex-col">
-                <h4 className="font-bold text-primary-600 tracking-wide">{paste.data.filename} </h4>
-                <span className="text-secondary-500 text-sm font-normal mt-1 truncate">
-                  {paste.data.description ? `(${paste.data.description})` : <br />}
+      {pastes.data.length > 0 ? (
+        pastes.data.map((paste: PasteQueryResponse, index) => (
+          <li
+            className="relative border-2 border-secondary-200 py-4 px-6 rounded hover:border-primary-300 flex flex-col"
+            key={index}
+          >
+            <Link href={`/p/${paste.data.pasteId}`}>
+              <a>
+                <div className="flex flex-col">
+                  <h4 className="font-bold text-primary-600 tracking-wide">{paste.data.filename} </h4>
+                  <span className="text-secondary-500 text-sm font-normal mt-1 truncate">
+                    {paste.data.description ? `(${paste.data.description})` : <br />}
+                  </span>
+                </div>
+              </a>
+            </Link>
+
+            <div className="text-secondary-500 absolute top-2 right-2">
+              <UserActions refid={paste.ref['@ref'].id} pasteid={paste.data.pasteId} />
+            </div>
+
+            <div className="mt-2 text-secondary-400">
+              <div className="flex items-center justify-between">
+                <span className="text-xs">
+                  {[
+                    `${
+                      paste.data.updated
+                        ? `${new Date(paste.data.updatedDate).toUTCString()}-updated`
+                        : new Date(paste.data.createdDate).toUTCString()
+                    }`
+                  ]}
+                </span>
+                <span className="bg-secondary-400 text-xs p-1 text-white ml-3">
+                  {paste.data.isPrivate ? 'private' : 'public'}
                 </span>
               </div>
-            </a>
-          </Link>
-
-          <div className="text-secondary-500 absolute top-2 right-2">
-            <UserActions refid={paste.ref['@ref'].id} pasteid={paste.data.pasteId} />
-          </div>
-
-          <div className="mt-2 text-secondary-400">
-            <div className="flex items-center justify-between">
-              <span className="text-xs">
-                {[
-                  `${
-                    paste.data.updated
-                      ? `${new Date(paste.data.updatedDate).toUTCString()}-updated`
-                      : new Date(paste.data.createdDate).toUTCString()
-                  }`
-                ]}
-              </span>
-              <span className="bg-secondary-400 text-xs p-1 text-white ml-3">
-                {paste.data.isPrivate ? 'private' : 'public'}
-              </span>
             </div>
-          </div>
-        </li>
-      ))}
+          </li>
+        ))
+      ) : (
+        <p className="text-secondary-600 tracking-wide text-sm">
+          You have not created any pastes yet! Try to create one and check it here!
+        </p>
+      )}
     </ul>
   );
 };
