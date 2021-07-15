406115d15a8e39d8542cc378b4a092b6f8ed292f
TheBoringDude <iamcoderx@gmail.com>
2021-07-15 09:42:20 +0800

    test: fix test in `home.spec.ts`

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -4,10 +4,10 @@ describe('Homepage', () => {
   });
 
   it('should display website name', () => {
-    cy.get('h1').contains('Local Paste');
+    cy.get('h1').contains('LCL Paste');
   });
 
   it('should get the website title', () => {
-    cy.get('head title').should('contain', 'Welcome  | Local Paste');
+    cy.get('head title').should('contain', 'Welcome  | LCL Paste');
   });
 });
