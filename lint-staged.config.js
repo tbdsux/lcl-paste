afbcffc238c6983ec76e1131e9f4adfd164f4bf8
TheBoringDude <iamcoderx@gmail.com>
2022-01-27 22:43:38 +0800

    [chore] Bump deps.
    - Move to `npm`

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // Run type-check on changes to TypeScript files
-  'src/**/*.ts?(x)': () => 'yarn type-check',
+  // Run type-check on changes to TypeScript files
+  'src/**/*.ts?(x)': () => 'npm run type-check',
   // Run ESLint on changes to JavaScript/TypeScript files
-  'src/**/*.(ts|js)?(x)': (filenames) => `yarn lint ${filenames.join(' ')}`,
-}
+  'src/**/*.(ts|js)?(x)': (filenames) => `npm run lint ${filenames.join(' ')}`
+};
